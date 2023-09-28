235d39c785acf8f197079c84fe97ed3469a7c35a
Guilherme <guilherme.sajs@gmail.com>
2023-09-28 05:02:18 -0300

    Adicionando sons aos eventos realizado no botão (começar)

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const botoes = document.querySelectorAll('.app__card-button')
 const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const musica = new Audio('./sons/luna-rise-part-one.mp3') 
+const audioPlay = new Audio('./sons/play.wav')
+const audioPausa = new Audio('./sons/pause.mp3')
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
 let tempoDecorridoEmSegundos = 5
 let intervaloId = null
@@ -66,8 +69,9 @@ function alterarContexto(contexto) {
 
 const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
-        zerar()
+        audioTempoFinalizado.play()
         alert('Tempo Finalizado!')
+        zerar()
         return
     }
     tempoDecorridoEmSegundos -= 1
@@ -78,9 +82,11 @@ startPauseBt.addEventListener('click', iniciarOuPausar)
 
 function iniciarOuPausar () {
     if(intervaloId) {
+        audioPausa.play()
         zerar()
         return
     }
+    audioPlay.play()
     intervaloId = setInterval(contagemRegressiva, 1000)
 }
 
